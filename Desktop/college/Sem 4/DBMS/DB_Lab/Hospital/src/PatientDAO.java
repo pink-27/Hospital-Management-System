bed6c2fc3c935089f1d744fc9e969a40b08b262f
pink-27 <98738847+pink-27@users.noreply.github.com>
2023-05-14 17:08:34 +0530

    Delete Desktop/college/Sem 4/DBMS/DB_Lab/Hospital directory

--- Desktop/college/Sem 4/DBMS/DB_Lab/Hospital/src/PatientDAO.java
+++
@@ -1,9 +0,0 @@
-import java.sql.Connection;
-import java.sql.Date;
-
-public interface PatientDAO {
-    public void RequestPrescription(String prescriptionID) throws Exception;
-
-    public void RequestAppointment(String patientID, String doctorID) throws Exception;
-
-}
